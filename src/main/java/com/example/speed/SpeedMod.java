6b8c50cf838b5128ed8766cffb4f452081ee9bdd
Kiserexser <towartors@mail.ru>
2026-06-18 14:24:31 +0300

    Update SpeedMod.java

--- src/main/java/com/example/speed/SpeedMod.java
+++ src/main/java/com/example/speed/SpeedMod.java
@@ -1,6 +1,7 @@
 package com.example.speed;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.lifecycle.v1.ClientLifecycleEvents;
 import net.minecraft.client.MinecraftClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,9 +13,10 @@ public class SpeedMod implements ModInitializer {
     public void onInitialize() {
         LOGGER.info("Speed Mod is running!");
 
-        // Показываем кастомный экран при запуске (только на клиенте)
-        if (MinecraftClient.getInstance() != null) {
-            MinecraftClient.getInstance().setScreen(new MainScreen());
-        }
+        // Регистрируем событие, которое запустится после полной загрузки клиента
+        ClientLifecycleEvents.CLIENT_STARTED.register(client -> {
+            // Теперь MinecraftClient существует и готов
+            client.setScreen(new MainScreen());
+        });
     }
 }
